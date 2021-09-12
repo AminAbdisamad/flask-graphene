a6e95c02bb6cb3c29c3b8e9c1c63c4b7527fd820
Amin Abdisamad <amin.abdisamad@b4finance.com>
2021-09-12 13:01:43 +0300

    new staff here

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from flask import Flask
 from schema import query_result, book_result
 
 app = Flask(__name__)
+# DB Config
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "postgresql://user:password@hostname/database_name"
 
 
 @app.get("/")
@@ -12,6 +16,11 @@ def index():
 @app.get("/book")
 def get_book():
     return {"data": book_result.data}
+
+
+@app.post("/")
+def index_post():
+    return "there we go"
 
 
 if __name__ == "__main__":
